0270be3d5a084d3925e82eaca3fcbdd8e817d510
Nikita Dema <104909568+NotFoundNone@users.noreply.github.com>
2024-05-30 19:40:35 +0300

    Create init-mirrors-win.sh

---
+++ init-mirrors-win.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+json_mirrors='{
+  "registry-mirrors" : [
+    "https://mirror.gcr.io",
+    "https://huecker.io",
+    "https://registry.docker-cn.com",
+    "https://daocloud.io"
+  ]
+}'
+
+# Записываем JSON в файл /etc/docker/daemon.json
+echo "$json_mirrors" > daemon.json
+
+# Перемещаем файл в /etc/docker/daemon.json
+mv daemon.json /etc/docker/daemon.json
+
+# Перезапускаем демон Docker
+service docker restart
